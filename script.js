e21d05b20d131116ef5bf60b5c5b3f706056f182
Mconway409 <70673286+Mconway409@users.noreply.github.com>
2020-12-10 16:35:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var apiKey = "7943510562302cd1b9e44fb5bf36fa6f";
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+var city="";
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
+
+
+
+
+
+
+
+
